1fa597e993ae8ae98f91d679d9b4e1a79e17fde6
Brandon <leebran@onid.oregonstate.edu>
2015-07-25 18:48:41 -0700

    Added instagram credentials

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 // Configure instagram app with client id
+ig.use({
+    client_id: '',
+    client_secret: ''
+});
 
 // Set the routes
 app.get('/', function(req, res) {
